7663d4766a14a22135d8d690c50676eb225cdeac
Aria <arctic0aurora@163.com>
2024-02-08 17:03:32 +0800

    furina fanfare simulation

--- characters/furina.py
+++ characters/furina.py
@@ -6,7 +6,72 @@ from optim import *
 
 import numpy as np
 
+
+# generate fanfare/duckweed axis in a rotation
+def fanfare_simulation(print_sequence=True):
+    # assumption: fanfare is counted after summons attack, healing is instant to regenerate lost hp
+    # (so that each time a summon consumes hp, fanfare is added twice)
+
+    small_hp, medium_hp, large_hp = 1.6, 2.4, 3.6
+    # 119 frame = 1.19s
+    small_cd, medium_cd, large_cd = 119, 290, 480
+    global_cd = 50 # applied only to small
+
+    scd, mcd, lcd, gcd = 0, 0, 0, 0 # cd counters
+    sequence = []
+    fanfare = 0
+    for i in range(0, 1800):
+        # small
+        if scd < 1 and gcd < 1:
+            sequence.append({
+                'type': 'small',
+                'timer': i,
+                'fanfare': fanfare,
+            })
+            fanfare += small_hp * 4 * 3.5 * 2
+            scd, gcd = small_cd, global_cd
+        if mcd < 1:
+            sequence.append({
+                'type': 'medium',
+                'timer': i,
+                'fanfare': fanfare,
+            })
+            fanfare += medium_hp * 4 * 3.5 * 2
+            mcd, gcd = medium_cd, global_cd
+        if lcd < 1:
+            sequence.append({
+                'type': 'large',
+                'timer': i,
+                'fanfare': fanfare,
+            })
+            fanfare += large_hp * 4 * 3.5 * 2
+            lcd, gcd = large_cd, global_cd
+        scd = scd - 1
+        mcd = mcd - 1
+        lcd = lcd - 1
+        gcd = gcd - 1
+
+    # simulation result
+    # small 15 (0-400:4, 400-800:4, 800-1500:7)
+    # medium 7 (2, 2, 3)
+    # large 4 (1, 1, 2)
+    if print_sequence:
+        print('fanfare axis in 1800 frames (18.0s)')
+        cnts = {'small':0, 'medium':0, 'large':0}
+        for action in sequence:
+            cnts[action['type']] += 1
+            print('{}: {}, fanfare={:.1f}'.format(action['timer'], action['type'], action['fanfare']))
+        print('action summary')
+        for typ in cnts:
+            print('{}: {} times'.format(typ, cnts[typ]))
+        print('')
+    return sequence
+
+
 class Furina(CharacterBase):
+
+    fanfare_sequence = fanfare_simulation()
+
     def __init__(self, weapon='jade', fanfare_weight=0.5, duckweed_weight=0.33):
         self.name = 'Furina'
         self.weapon = weapon
@@ -22,17 +87,34 @@ class Furina(CharacterBase):
             'em': 0,
         }
         self.attrs = self.construct_attrs(furina_base)
-        self.requirement = {
-            'thres': 0,
-            'set_restriction': {'goldentroupe': 4}
+        self.mult = {
+            'solitaire-bubble': 14.2, # ousia e release
+            'small': 5.82, # surintendante chevalmarin
+            'medium': 10.73, # gentilhomme usher
+            'large': 14.92, # mademoiselle crabaletta
+            'rejoice': 20.5, # q release
+            'love-chalice': 18, # furina c6
+            'love-chalice-pneuma': 25, # furina c6 pneuma
         }
+        self.requirement = {
+            'set-type': '4pcs',
+            'set-restriction': ['goldentroupe'],
+        }
+        self.recharge_thres = 150 # randomly set in favor of recharge hourglass
         self.artifacts = ArtifactCollection([])
         self.apply_weapon()
         # self.apply_team()
 
     # furina talent2
-    def confession_bns(self, duckweed_hp=0):
+    def confession_bonus(self, duckweed_hp=0):
         return min(28, (self.hp()+duckweed_hp)*0.7/1000)
+    
+    # fanfare to buff
+    def fanfare_bonus(self, fanfare):
+        return 0.25 * min(fanfare, 400)
+    
+    def fanfare_hp(self, fanfare):
+        return max(0, 0.35 * (min(fanfare, 800) - 400))
     
     def apply_weapon(self):
         if self.weapon == 'tranquil':
@@ -41,6 +123,8 @@ class Furina(CharacterBase):
             self.apply_misugiri()
         elif self.weapon == 'jade':
             self.apply_primodial_jade()
+        elif self.weapon == 'favonius':
+            self.apply_favonius()
     
     def apply_tranquil(self):
         self.apply_modifier('atk0', 542)
@@ -61,6 +145,10 @@ class Furina(CharacterBase):
         self.apply_modifier('atk0', 542)
         self.apply_modifier('cr', 44.1)
         self.apply_modifier('H', 20)
+
+    def apply_primodial_jade(self):
+        self.apply_modifier('atk0', 454)
+        self.apply_modifier('rcg', 61.3)
 
     def apply_artifacts(self, artifacts):
         super().apply_artifacts(artifacts)
@@ -100,10 +188,9 @@ class Furina(CharacterBase):
 
 
     def optim_target(self, team=['kazuha', 'kokomi', 'yelan'], args=['recharge_thres']):
-        # rotation e summon damage as feature
-        # assumption: fanfare is counted after summons attack, healing is instant to regenerate lost hp
+        # rotation e summon damage as feature  
 
-        if 'recharge_thres' in args and self.rcg() < 150:
+        if 'recharge_thres' in args and self.rcg() < self.recharge_thres:
             return Composite(), {}
 
         ousia_bubble = 14.2
@@ -167,3 +254,11 @@ class Furina(CharacterBase):
                          'surintendante_chevalmarin(duckweed)': small_c2full, 
                          'gentilhomme_usher(duckweed)': medium_c2full, 
                          'mademoiselle_crabaletta(duckweed)': large_c2full}
+
+
+
+
+
+
+
+
